1025c71e1196e822e967796219b895b626173bab
Elliot Cobb <elliot@liveclicker.com>
2015-08-25 13:51:29 -0600

    added individual photos

--- theinstitute/js/index.js
+++ theinstitute/js/index.js
@@ -22,10 +22,12 @@ $('.the-team-content .thumbnail')
         //$(this).find('.curtain').show();
         $(this).find('.headshot').addClass('shaded-headshot');
         $(this).find('.nametag').show();
+        $(this).find('.headshot2').show();
     })
     .mouseleave( function () {
         $(this).find('.headshot').removeClass('shaded-headshot');
         $(this).find('.nametag').hide();
+        $(this).find('.headshot2').hide();
     })
 ;
 
