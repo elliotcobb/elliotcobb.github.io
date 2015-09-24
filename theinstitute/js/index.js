a0d83af4c2c93d4c7aecf90580051419a6427e60
Elliot Cobb <elliot@liveclicker.com>
2015-09-24 16:14:50 -0400

    randomized thumbnails

--- theinstitute/js/index.js
+++ theinstitute/js/index.js
@@ -2,7 +2,7 @@
  * Created by elliotcobb on 8/21/15.
  */
 
-$('.the-team-content .thumbnail')
+$('.the-team-content-holder .thumbnail')
     .mouseenter( function() {
         //$(this).find('.curtain').show();
         $(this).find('.headshot').addClass('shaded-headshot');
@@ -16,7 +16,7 @@ $('.the-team-content .thumbnail')
     })
 ;
 
-$('.the-team-content .thumbnail').click(function() {
+$('.the-team-content-holder .thumbnail').click(function() {
     console.log("click");
     var bio = $(this).find('.headshot-bio');
     if (bio.css('display') == 'none') {
@@ -26,4 +26,32 @@ $('.the-team-content .thumbnail').click(function() {
         // hide
         bio.hide();
     }
+});
+
+function shuffleThumbs() {
+    var thumbArray = new Array();
+    thumbArray = assignThumbsRandomVals(thumbArray);
+    thumbArray = thumbArray.sort();
+    removeAppendSortedThumbs(thumbArray);
+}
+
+function assignThumbsRandomVals(thumbArray) {
+    $('.thumbColumn[data-randID]').each( function() {
+        var randomID = Math.floor((Math.random() * 10000) + 1);
+        thumbArray.push(randomID);
+        $(this).attr('data-randID', randomID);
+    });
+    return thumbArray;
+}
+
+function removeAppendSortedThumbs(thumbArray) {
+    for (var i = 0; i < thumbArray.length; i++) {
+        $('.thumbColumn[data-randID=' + thumbArray[i] + ']').appendTo('.the-team-content');
+    }
+};
+
+
+
+$(document).ready( function() {
+    shuffleThumbs();
 });
